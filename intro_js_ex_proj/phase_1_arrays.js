2e7995b2eef6c9416871d0327f6214f44cc7f5a4
Nick <nbarrameda89@gmail.com>
2021-11-23 15:20:41 -0500

    twoSum complete

--- intro_js_ex_proj/phase_1_arrays.js
+++ intro_js_ex_proj/phase_1_arrays.js
@@ -1,12 +1,10 @@
 
 let a = [1,2,2,3,3,3]
 
-debugger
 Array.prototype.uniq = function () {
     let result = [];
 
-    for (let i = 0; i < this.length-1; i++) {
-        debugger
+    for (let i = 0; i <= this.length-1; i++) {
         if (!result.includes(a[i])) { result.push(this[i]) };
     }
 
@@ -14,3 +12,22 @@ Array.prototype.uniq = function () {
 }
 
 console.log(a.uniq())
+
+let b = [-2, -1, 0, 2]
+
+Array.prototype.twoSum = function () {
+    let result = [];
+    
+    for (let i = 0; i <= this.length-1; i++) {
+        for (let j = i + 1; j <= this.length-1; j++) {
+            if ((this[i] + this[j]) === 0) {
+                let netZeroPair = [this[i], this[j]]
+                result.push(netZeroPair);
+            }
+        }
+    }
+    return result;
+}
+
+console.log(b.twoSum())
+
